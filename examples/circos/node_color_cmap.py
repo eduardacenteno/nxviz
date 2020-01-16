1254eaeaa68baf3749e904b73a5205697fadd791
Rabeez Riaz <19100165@lums.edu.pk>
2020-01-16 14:57:55 +0500

    Added examples showcasing node_cmap

---
+++ examples/circos/node_color_cmap.py
@@ -0,0 +1,24 @@
+"""
+Displays different node_color based on a provided cmap with ArcPlot
+"""
+
+import matplotlib.pyplot as plt
+import networkx as nx
+from nxviz.plots import CircosPlot
+
+G = nx.Graph()
+
+G.add_node("A", type="Alpha")
+G.add_node("B", type="Beta")
+G.add_node("C", type="Alpha")
+
+
+G.add_edge("A", "B", weight=8)
+G.add_edge("A", "C", weight=8)
+G.add_edge("B", "C", weight=8)
+
+
+c = CircosPlot(G, node_color="type", node_cmap="Set1", edge_width="weight")
+
+c.draw()
+plt.show()
